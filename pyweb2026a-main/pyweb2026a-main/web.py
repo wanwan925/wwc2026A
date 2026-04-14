a624fc660d3ee6bfea1a611ff5d98277a8089172
wanwan925 <n0975755006@gmail.com>
2026-04-14 12:23:59 +0800

    4/14

--- pyweb2026a-main/pyweb2026a-main/web.py
+++ pyweb2026a-main/pyweb2026a-main/web.py
@@ -1,22 +1,3 @@
-import os
-import json
-import firebase_admin
-from firebase_admin import credentials, firestore
-from google.cloud.firestore_v1.base_query import FieldFilter
-
-# 判斷是在 Vercel 還是本地
-if os.path.exists('serviceAccountKey.json'):
-    # 本地環境：讀取檔案
-    cred = credentials.Certificate('serviceAccountKey.json')
-else:
-    # 雲端環境：從環境變數讀取 JSON 字串
-    firebase_config = os.getenv('FIREBASE_CONFIG')
-    cred_dict = json.loads(firebase_config)
-    cred = credentials.Certificate(cred_dict)
-
-firebase_admin.initialize_app(cred)
-
-
 from flask import Flask,render_template, request
 from datetime import datetime
 import random
@@ -32,59 +13,7 @@ def index():
     link += "<a href=/account>POST傳值(帳號密碼)</a><hr>"
     link += "<a href=/math>數學運算</a><hr>"
     link += "<a href=/cup>擲茭</a><hr>"
-    link += "<a href=/read>讀取Firestore資料(根據lab遞減排序，取前4)</a><br>"
-    link += "<a href=/search>查詢老師研究室</a><hr>"
     return link
-
-@app.route("/read")
-def read():
-    db = firestore.client()
-
-    Temp = ""
-    collection_ref = db.collection("靜宜資管2026a")
-    docs = collection_ref.order_by("lab",direction=firestore.Query.DESCENDING).limit(4).get()
-    for doc in docs:
-        Temp += str(doc.to_dict()) + "<br>"
-
-    Temp += "<br><a href='/'>回網站首頁</a>"
-
-    return Temp 
-
-
-@app.route("/search", methods=["GET", "POST"])
-def search():
-    if request.method == "POST":
-        keyword = request.form["keyword"]
-        db = firestore.client()
-        collection_ref = db.collection("靜宜資管2026a")
-        docs = collection_ref.get()
-        
-        result = f"您查詢的關鍵字是：{keyword}<br><br>"
-        found = False
-        for doc in docs:
-            user = doc.to_dict()
-            if keyword in user.get("name", ""):
-                result += f"{user['name']} 老師的研究室在 {user['lab']}<br>"
-                found = True
-        
-        if not found:
-            result += "抱歉，找不到符合條件的老師。"
-            
-        result += "<br><a href='/search'>重新查詢</a> | <a href='/'>回到首頁</a>"
-        return result
-    else:
-        # 顯示查詢表單
-        html = """
-            <h1>查詢老師研究室</h1>
-            <form action="/search" method="post">
-                請輸入要查詢的老師姓名關鍵字：<br>
-                <input type="text" name="keyword">
-                <input type="submit" value="查詢">
-            </form>
-            <br><a href="/">回到首頁</a>
-        """
-        return html
-
 
 @app.route("/mis")
 def course():
