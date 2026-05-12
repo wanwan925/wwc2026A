8bb020d42b54ef500f1878be8b810b5e3a068550
wanwan925 <n0975755006@gmail.com>
2026-05-12 11:52:54 +0800

    Add files via upload

--- web.py
+++ web.py
@@ -57,7 +57,7 @@ def webhook():
         rate =  req["queryResult"]["parameters"]["rate"]
         info = "我是吳菀秦設計的電影聊天機器人,您選擇的電影分級是：" + rate + "，相關電影：\n"
 
-    db = firestore.client()
+        db = firestore.client()
         collection_ref = db.collection("本週新片含分級")
         docs = collection_ref.get()
         result = ""
