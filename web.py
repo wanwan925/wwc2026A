46616a382abb464d52491a74c962e9a1cd82c5b6
wanwan925 <n0975755006@gmail.com>
2026-05-26 11:24:00 +0800

    ai01

--- web.py
+++ web.py
@@ -107,6 +107,10 @@ def webhook():
                 result += "片名：" + dict["title"] + "\n"
                 result += "介紹：" + dict["hyperlink"] + "\n\n"
         info += result
+
+    elif (action == "input.unknown"):
+        info =  req["queryResult"]["queryText"]
+
     return make_response(jsonify({"fulfillmentText": info}))
 
 @app.route("/rate")
