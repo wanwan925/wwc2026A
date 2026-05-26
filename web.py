93f31f87987b0fb773ce47b57b096dce6aad6028
wanwan925 <n0975755006@gmail.com>
2026-05-26 11:50:32 +0800

    Add files via upload

--- web.py
+++ web.py
@@ -114,7 +114,7 @@ def webhook():
 
         # 2. 建立設定物件，設定你希望限制的最大 Token 數（例如 500）
         ai_config = types.GenerateContentConfig(
-            max_output_tokens = 2000
+            max_output_tokens = 500
         )
 
 
